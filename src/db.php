113165c5278f06f5fe6e5a102ba1b1cc2b64640d
vincent <vincent.garry@hotmail.com>
2024-07-30 10:04:25 +0200

    modif db.php

--- src/db.php
+++ src/db.php
@@ -1,9 +1,9 @@
 <?php
 function connectDatabase() {
-    $servername = getenv('MYSQL_HOST');
-    $username = getenv('MYSQL_USER');
-    $password = getenv('MYSQL_PASSWORD');
-    $dbname = getenv('MYSQL_DB');
+    $servername = "db";
+    $username = "root";
+    $password = "DSPProject2024";
+    $dbname = "applications";
 
     // Créer une connexion
     $conn = new mysqli($servername, $username, $password, $dbname);
