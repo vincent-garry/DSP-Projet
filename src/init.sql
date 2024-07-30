09420db8b83815bea25f5fdefc00a1300dbc9d6d
vincent <vincent.garry@hotmail.com>
2024-07-30 11:31:07 +0200

    test

--- src/init.sql
+++ src/init.sql
@@ -1,15 +1,8 @@
-CREATE DATABASE IF NOT EXISTS applications;
 USE applications;
 
-CREATE TABLE IF NOT EXISTS your_table_name (
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
--- Insert some test data
-INSERT INTO your_table_name (name) VALUES ('Test Name 1'), ('Test Name 2');
-
--- Create user if not exists and grant privileges
-CREATE USER IF NOT EXISTS 'webuser'@'%' IDENTIFIED BY 'webpass';
-GRANT ALL PRIVILEGES ON applications.* TO 'webuser'@'%';
-FLUSH PRIVILEGES;
+INSERT INTO users (name) VALUES ('John Doe'), ('Jane Doe');
