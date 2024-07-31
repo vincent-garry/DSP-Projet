2df13b0cc83b88985bbff66289742fe3a522c046
vincent <vincent.garry@hotmail.com>
2024-07-31 16:33:23 +0200

    test tic tac toe

--- Dockerfile
+++ Dockerfile
@@ -4,25 +4,8 @@ FROM node:14
 # Créer et définir le répertoire de l'application dans le conteneur
 WORKDIR /app
 
-# Copier package.json et package-lock.json
-COPY package*.json ./
-
-# Lister les fichiers pour vérifier la copie
-RUN ls -la /app
-
-# Installer les dépendances
-RUN npm install
-
-# Lister les fichiers pour vérifier l'installation des dépendances
-RUN ls -la /app/node_modules
-
-# Copier le reste de votre application
 COPY src .
 
-# Lister les fichiers pour vérifier la copie
-RUN ls -la /app
-
-# Exposer le port sur lequel l'application s'exécute (par exemple, 3000)
 EXPOSE 3000
 
 # Commande pour lancer l'application
--- src/server.js
+++ src/server.js
@@ -1,31 +1,57 @@
-const express = require("express");
-const cors = require("cors");
+var connections = {};
+var games = {};
+var url = require('url');
 
-const app = express();
-// Routes
-app.use(express.static("public"));
-app.use(cors());
+var send = function(res, data){
+	headers = {
+		'Content-type': 'application/json',
+		'Access-Control-Allow-Headers': 'Content-type',
+		'Access-Control-Allow-Origin': '*'
+	}
+	message = JSON.stringify(data);
+	headers['Contnent-length'] = message.length;
+	
+	res.writeHead(200, headers);
+	res.end(message);
+}
 
-app.get("/", (req, res) => {
-  res.sendFile("/app/src/calculette.html");
-});
-app.get("/app.js", (req, res) => {
-  res.sendFile("/app/src/app.js");
-});
-app.get("/app.css", (req, res) => {
-  res.sendFile("/app/src/app.css");
-});
+var on_request = function(request,response){
+	var body = '';
+	if (request.method != 'POST') {
+		send(response, null);
+		return;
+	}
+	request.on('data', function (data) {
+		body += data;
+	});
+	request.on('end', function(){
+		var params = JSON.parse(body);
+	
+		var room = params.room;
+		connections[room] = connections[room] || [];
+		games[room] = games[room] || false;
+		if (params.command == 'init') {
+			if (connections[room].length == 1) {
+				games[room] = true;
+				send(connections[room][0], {player: 1});
+				send(response, {player: 2});
+				return;
+			}
+			if (games[room]) {
+				send(response, {player: -1});
+			}
+		}
+		// received command other than init, so game must be started
+		games[room] = true; 
+		if (params.command == 'set') {
+			for (var i = connections[room].length -1; i >= 0; i--) {
+				res = connections[room][i];
+				send(res, params);
+				connections[room].splice(i,1);
+			}
+		}
+		connections[room].push(response);
+	});
+}
 
-// app.get("/", (req, res) => {
-//   res.sendFile("C:/xampp/php/www/DSP-projet/src/calculette.html"); // Remplacez 'index.html' par votre fichier HTML principal
-// });
-// app.get("/app.js", (request, response) => {
-//   response.sendFile("C:/xampp/php/www/DSP-projet/src/app.js");
-// });
-// app.get("/app.css", (request, response) => {
-//   response.sendFile("C:/xampp/php/www/DSP-projet/src/app.css");
-// });
-
-https: app.listen(3000, () => {
-  console.log(`Server listening on port 3000`);
-});
+var http = require('http').createServer(on_request).listen(8080);
