5c3f5adfe5c4947b0bbcd5f43b196d2e3a8c2ac0
Patrice-Emile <76409884+Patrice-Emile@users.noreply.github.com>
2024-07-31 14:18:11 +0200

    test

--- src/server.js
+++ src/server.js
@@ -1,18 +1,18 @@
-let express = require('express');
-let cors = require('cors');
+let express = require("express");
+let cors = require("cors");
 
 const app = express();
-let rootPath = '/home/tom/Documents/0-SIMPLON/projet_calculatrice/';
-app.get('/' ,(request, response) => {
-    response.sendFile(rootPath +'calculette.html');
+let rootPath = "/app/src/";
+app.get("/", (request, response) => {
+  response.sendFile(rootPath + "calculette.html");
 });
-app.get('/app.js' ,(request, response) => {
-    response.sendFile(rootPath +'app.js');
+app.get("/app.js", (request, response) => {
+  response.sendFile(rootPath + "app.js");
 });
-app.get('/app.css' ,(request, response) => {
-    response.sendFile(rootPath +'app.css');
+app.get("/app.css", (request, response) => {
+  response.sendFile(rootPath + "app.css");
 });
 
-app.listen(8090, () => {
-    console.log(`Server listening on port 8090`)
+app.listen(8080, () => {
+  console.log(`Server listening on port 8090`);
 });
