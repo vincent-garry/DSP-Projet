4754923e3c0be0fb93133e603244b56619f77e1c
Patrice-Emile <76409884+Patrice-Emile@users.noreply.github.com>
2024-07-31 14:59:11 +0200

    test

--- src/server.js
+++ src/server.js
@@ -1,18 +1,32 @@
-let express = require("express");
-let cors = require("cors");
+const express = require("express");
+const path = require("path"); // Importez le module path
+const cors = require("cors");
 
 const app = express();
-let rootPath = "/app/src/";
-app.get("/", (request, response) => {
-  response.sendFile(rootPath + "calculette.html");
+// Routes
+app.use(express.static("public"));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join("app/src/calculette.html")); // Remplacez 'index.html' par votre fichier HTML principal
 });
 app.get("/app.js", (request, response) => {
-  response.sendFile(rootPath + "app.js");
+  response.sendFile("app/src/app.js");
 });
 app.get("/app.css", (request, response) => {
-  response.sendFile(rootPath + "app.css");
+  response.sendFile("app/src/app.css");
 });
 
-app.listen(8080, () => {
-  console.log(`Server listening on port 8090`);
+// app.get("/", (req, res) => {
+//   res.sendFile("C:/xampp/php/www/DSP-projet/src/calculette.html"); // Remplacez 'index.html' par votre fichier HTML principal
+// });
+// app.get("/app.js", (request, response) => {
+//   response.sendFile("C:/xampp/php/www/DSP-projet/src/app.js");
+// });
+// app.get("/app.css", (request, response) => {
+//   response.sendFile("C:/xampp/php/www/DSP-projet/src/app.css");
+// });
+
+https: app.listen(3000, () => {
+  console.log(`Server listening on port 3000`);
 });
