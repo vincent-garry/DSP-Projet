e88f6f489677a39f2eba0b5082916fd89b652897
vincent <vincent.garry@hotmail.com>
2024-07-31 10:14:36 +0200

    test

--- src/Package.swift
+++ src/Package.swift
@@ -3,13 +3,11 @@ import PackageDescription
 
 let package = Package(
     name: "P3_Battle",
-    dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-    ],
+    dependencies: [],
     targets: [
         .executableTarget(
             name: "P3_Battle",
-            dependencies: [.product(name: "Vapor", package: "vapor")],
+            dependencies: [],
             path: "Sources"
         ),
 
